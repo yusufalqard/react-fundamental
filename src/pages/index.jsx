7c0ff332f793eb518a82f2a40073ab12b0d31d54
yusufalqard <yusuf.alqard@gmail.com>
2025-09-22 19:01:50 +0700

    add: explanation

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,10 @@ function Home() {
     <>
       <h1>Simple Portofolio Blog</h1>
       {posts.map((blog) => {
+        // return <div>{blog.title}, {blog.tags}, {blog.date}</div>;
+        // Line 9 are work as intended
         <Article title={blog.title} date={blog.date} tags={blog.tags} />;
+        // Line 11 doesnt show any imported array from Article.jsx
       })}
     </>
   );
