af7e572ff16d5336cdc046defbebb5a95aa0d0fb
yusufalqard <yusuf.alqard@gmail.com>
2025-09-24 22:47:10 +0700

    rev: add feature useState and revise posts.map

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import posts from "../posts.json";
 import Article from "../components/Article";
 
+
 function Home() {
+  const [DataSearch, setDataSearch] = useState("");
+
+  const FindChanges = (event) => {
+    setDataSearch = event.target.value;
+  };
   return (
     <>
       <h1>Simple Portofolio Blog</h1>
-      {posts.map((blog) => {
+      <div>
+        Find Article : <input onChange={FindChanges}></input>{" "}
+      </div>
+      <small>Ditemukan 0 data dengan pencarian kata {DataSearch}</small>
+      {posts.map(({title, tags, date}, index) => {
+        // Line 19 changes from posts.map((blog) into newest
         // return <div>{blog.title}, {blog.tags}, {blog.date}</div>;
-        // Line 9 are work as intended
-        <Article title={blog.title} date={blog.date} tags={blog.tags} />;
-        // Line 11 doesnt show any imported array from Article.jsx
+        // Line 21 are work as intended
+        // <Article title={blog.title} date={blog.date} tags={blog.tags} />;
+        // Line 23 doesnt show any imported array from Article.jsx
+        <Article {...{title, tags, date}} key={index} />
       })}
     </>
   );
